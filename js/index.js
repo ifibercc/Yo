4119c0d4b4c8039b24ee6f411dbbb9511ed06c22
Yu Feng <yuvon1990@gmail.com>
2016-03-08 11:47:45 +0800

    Yu

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
-$(function(){
+$(document).ready(function() {
     $('#fullpage').fullpage({
-        verticalCentered: false,
+        // verticalCentered: false,
         css3: true,
-        sectionsColor: ['#f2f2f2', '#fff', '#f2f2f2', '#fff'],
-        anchors: ['page1', 'page2', 'page3', 'page4'],
-        navigation: true,
-        navigationPosition: 'right',
-        navigationColor: '#000',
+        sectionsColor: ['#f2f2f2', '#ddd', '#f2f2f2', '#fff'],
+        // anchors: ['page1', 'page2', 'page3', 'page4'],
+        // navigation: true,
+        // navigationPosition: 'right',
+        // navigationColor: '#000',
         navigationTooltips: ['page1', 'page2', 'page3', 'page4'],
         slidesNavigation: true,
-        slidesNavPosition: top,
-        controlArrowColor: '#000'
+        // slidesNavPosition: top,
+        // controlArrowColor: '#000'
     });
 });
