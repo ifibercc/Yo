69895c82218b6187e964bda0365f3ff7918691f1
Lemoo <ifibercc@gmail.com>
2016-03-08 13:49:47 +0800

    add timeline plugin

--- js/index.js
+++ js/index.js
@@ -1,15 +1,40 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    // fullpage
     $('#fullpage').fullpage({
-        // verticalCentered: false,
+        resize: true,
         css3: true,
         sectionsColor: ['#f2f2f2', '#ddd', '#f2f2f2', '#fff'],
-        // anchors: ['page1', 'page2', 'page3', 'page4'],
-        // navigation: true,
-        // navigationPosition: 'right',
-        // navigationColor: '#000',
+        anchors: ['page1', 'page2', 'page3', 'page4'],
+        // 上下滑块配置
+        navigation: false,
+        navigationPosition: 'right',
+        navigationColor: '#000',
         navigationTooltips: ['page1', 'page2', 'page3', 'page4'],
-        slidesNavigation: true,
-        // slidesNavPosition: top,
-        // controlArrowColor: '#000'
+
+        controlArrowColor: '#000',
+        loopHorizontal: true
+    });
+
+    // timeline
+    var ev = [
+        {
+            id: 1,
+            name: "Yo团队组建成功",
+            on: new Date(2016, 2, 5)
+        },
+        {
+            id: 2,
+            name: "Yo团队在GitHub第一次提交代码",
+            on: new Date(2016, 2, 8)
+        }
+    ];
+    $('#longTimeLine').jqtimeline({
+        events: ev,
+        numYears: 1,
+        startYear: 2016,
+        gap: 150,
+        click: function (e, event) {
+            //loadPage(event);
+        }
     });
 });
