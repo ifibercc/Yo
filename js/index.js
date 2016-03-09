cc98cff75589faaecb05422e0081a8e5682637cf
Lemoo <ifibercc@gmail.com>
2016-03-09 11:50:48 +0800

    change timeline plugin

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     $('#fullpage').fullpage({
         resize: true,
         css3: true,
-        sectionsColor: ['#f2f2f2', '#ddd', '#f2f2f2', '#fff'],
+        sectionsColor: ['#f2f2f2', '#ddd', '#f2f2f2', '#aeaeae'],
         anchors: ['page1', 'page2', 'page3', 'page4'],
         // 上下滑块配置
         navigation: false,
@@ -16,5 +16,23 @@ $(document).ready(function () {
     });
 
     // timeline
+    var ev = [{
+        id : 1,
+        name : 'Yo团队组建成功',
+        on : new Date(2016,2,4)
+    },{
+        id : 2,
+        name : 'Yo团队第一次在GitHub提交代码',
+        on : new Date(2016,2,8)
+    }];
+    $('#longTimeLine').jqtimeline({
+        events : ev,
+        numYears: 1,
+        startYear: 2016,
+        gap: 100,
+        click:function(e,event){
+            //loadPage(event);
+        }
+    });
 
 });
